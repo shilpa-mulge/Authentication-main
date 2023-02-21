a0a25cee20e6004d147eb154483aa2044a814ab9
Shilpa <mulgeshilpa@gmail.com>
2023-02-21 14:34:09 +0530

    Login page

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -15,29 +15,59 @@ const AuthForm = () => {
     setIsCliked(true)
     const enterdEmail = enterdEmailRef.current.value;
     const entardPassword = entardPasswordRef.current.value;
-    fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB2IbR8h8-w-hfsXzEWYgYExp3fG4R8PQ8", {
-      method: 'POST',
-      body: JSON.stringify({
-        email: enterdEmail, password: entardPassword, returnSecureToken: true
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then((response) => {
-      setIsCliked(false)
-      if (response.ok) {
+    if (isLogin) {
+      fetch("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB2IbR8h8-w-hfsXzEWYgYExp3fG4R8PQ8", {
+        method: 'POST',
+        body: JSON.stringify({
+          email: enterdEmail, password: entardPassword, returnSecureToken: true
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then((response) => {
+        setIsCliked(false)
+        if (response.ok) {
+          return response.json().then(data => {
+            console.log(data)
+          })
+        } else {
+          return response.json().then(data => {
+            if (data && data.error && data.error.message) {
+              window.alert(data.error.message)
+            }
+            else {
+              alert("Authentication faild!")
+            }
+          })
+        }
+      })
+    }
+    else {
+      fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB2IbR8h8-w-hfsXzEWYgYExp3fG4R8PQ8", {
+        method: 'POST',
+        body: JSON.stringify({
+          email: enterdEmail, password: entardPassword, returnSecureToken: true
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then((response) => {
+        setIsCliked(false)
+        if (response.ok) {
 
-      } else {
-        return response.json().then(data => {
-          if (data && data.error && data.error.message) {
-            window.alert(data.error.message)
-          }
-          else {
-            alert("Authentication faild!")
-          }
-        })
-      }
-    })
+        } else {
+          return response.json().then(data => {
+            if (data && data.error && data.error.message) {
+              window.alert(data.error.message)
+            }
+            else {
+              alert("Authentication faild!")
+            }
+          })
+        }
+      })
+
+    }
 
 
   }
